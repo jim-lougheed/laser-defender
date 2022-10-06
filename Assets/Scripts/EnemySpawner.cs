c1d3da713ac2d54410ece71681daa057e408435c
Jim Lougheed <jims.codes@gmail.com>
2022-10-06 14:40:49 -0400

    Loop through waves using do-while

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
     WaveConfigSO currentWave;
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping = true;
     void Start()
     {
         StartCoroutine(SpawnEnemyWaves());
@@ -14,14 +15,18 @@ public class EnemySpawner : MonoBehaviour
     }
 
     IEnumerator SpawnEnemyWaves() {
-        foreach (WaveConfigSO wave in waveConfigs) {
-            currentWave = wave;
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++) {
-                Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+        do {
+            foreach (WaveConfigSO wave in waveConfigs) {
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++) {
+                    Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
         }
+        while (isLooping);
+        
     }
 
     public WaveConfigSO GetCurrentWave() {
