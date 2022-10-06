eb820545fd849d25f75c940a44e89916e1f2b7ce
Jim Lougheed <jims.codes@gmail.com>
2022-10-06 14:07:19 -0400

    Configure 2 waves

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] WaveConfigSO currentWave;
+    WaveConfigSO currentWave;
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
     void Start()
     {
-        SpawnEnemies();
+        StartCoroutine(SpawnEnemyWaves());
+        Debug.Log(waveConfigs);
     }
 
-    void SpawnEnemies() {
-        for (int i = 0; i < currentWave.GetEnemyCount(); i++) {
-            Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
+    IEnumerator SpawnEnemyWaves() {
+        foreach (WaveConfigSO wave in waveConfigs) {
+            currentWave = wave;
+            for (int i = 0; i < currentWave.GetEnemyCount(); i++) {
+                Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWayPoint().position, Quaternion.identity, transform);
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+            }
+            yield return new WaitForSeconds(timeBetweenWaves);
         }
     }
 
--- Assets/Scripts/WaveConfigSO.cs
+++ Assets/Scripts/WaveConfigSO.cs
@@ -8,6 +8,9 @@ public class WaveConfigSO : ScriptableObject
     [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance = 0f;
+    [SerializeField] float minSpawnTime = 0.2f;
 
     public float GetMoveSpeed() {
         return moveSpeed;
@@ -31,5 +34,10 @@ public class WaveConfigSO : ScriptableObject
             waypoints.Add(child);
         }
         return waypoints;
+    }
+
+    public float GetRandomSpawnTime() {
+        float spawnTime = Random.Range(timeBetweenEnemySpawns - spawnTimeVariance, timeBetweenEnemySpawns + spawnTimeVariance);
+        return Mathf.Clamp(spawnTime, minSpawnTime, float.MaxValue);
     }
 }
