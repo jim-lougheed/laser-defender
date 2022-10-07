fec6345c9e33d98d1666b20ef5ecf67c072466e2
Jim Lougheed <jims.codes@gmail.com>
2022-10-07 15:15:19 -0400

    Add particle effects for hits

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
+    [SerializeField] ParticleSystem hitEffect;
 
     void OnTriggerEnter2D(Collider2D other) {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
         if (damageDealer != null) {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
             damageDealer.Hit();
         }
     }
@@ -18,6 +20,13 @@ public class Health : MonoBehaviour
         health -= damageTaken;
         if (health <= 0) {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect() {
+        if (hitEffect != null) {
+            ParticleSystem instance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(instance.gameObject, instance.main.duration + instance.main.startLifetime.constantMax);
         }
     }
 }
